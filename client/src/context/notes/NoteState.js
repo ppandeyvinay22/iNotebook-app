1ea9b2fd176ad24ef2ef5e11bf89d2590f5df7ae
Vinay Kumar Pandey <88609151+ppandeyvinay22@users.noreply.github.com>
2023-08-13 16:41:13 +0530

    Update NoteState.js

--- client/src/context/notes/NoteState.js
+++ client/src/context/notes/NoteState.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 const NoteState = (props) => {
 
-  const host = "http://localhost:5000"
+  const host = "https://inotebook-app-mern.vercel.app"
 
   const notesInitial = [];
 
