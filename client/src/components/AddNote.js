19ae55ebed421ecd8e80a8509844900bb1ab7c44
ppandeyvinay22 <ppandeyvinay22@gmail.com>
2023-08-15 15:48:29 +0530

    tag changed to status

--- client/src/components/AddNote.js
+++ client/src/components/AddNote.js
@@ -1,21 +1,21 @@
-import React, {useContext, useState} from 'react'
+import React, { useContext, useState } from 'react'
 import NoteContext from '../context/notes/NoteContext'
 
 const AddNote = (props) => {
     const context = useContext(NoteContext);
     const { addNote } = context;
-    
-    const [note, setNote] = useState({title: "", description: "", tag:""})
 
-    const handleClick = (e) =>{
+    const [note, setNote] = useState({ title: "", description: "", status: "" })
+
+    const handleClick = (e) => {
         e.preventDefault();
-        addNote(note.title, note.description, note.tag);
-        setNote({title: "", description: "", tag:""});
+        addNote(note.title, note.description, note.status);
+        setNote({ title: "", description: "", status: "" });
         props.showAlert("Added Successfully", "success");
     }
 
     const onChange = (e) => {
-        setNote({...note, [e.target.name]: e.target.value})
+        setNote({ ...note, [e.target.name]: e.target.value })
     }
 
     return (
@@ -35,7 +35,7 @@ const AddNote = (props) => {
                     <label htmlFor="status" className="form-label">Status</label>
                     <input type="text" className="form-control" id="status" name='status' value={note.status} onChange={onChange} minLength={5} required />
                 </div>
-                <button disabled={note.title.length <5 || note.description.length <5} type="submit" className="btn btn-primary btn-sm" onClick={handleClick}>Add Task</button>
+                <button disabled={note.title.length < 5 || note.description.length < 5} type="submit" className="btn btn-primary btn-sm" onClick={handleClick}>Add Task</button>
             </form>
         </div>
     )
